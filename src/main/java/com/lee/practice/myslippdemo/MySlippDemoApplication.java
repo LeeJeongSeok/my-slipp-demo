5d8ef7b7bca31ac826b09aa4be8db46074a36d33
LeeJeongSeok <ljs0429777@gmail.com>
2020-07-18 00:37:59 +0900

    Initial commit

---
+++ src/main/java/com/lee/practice/myslippdemo/MySlippDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lee.practice.myslippdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySlippDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lee/practice/myslippdemo/MySlippDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.practice.myslippdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySlippDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
