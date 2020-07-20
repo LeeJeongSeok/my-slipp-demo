b1c6f82b8a38affd97bbbb16d6fe5d71b3dc59ed
LeeJeongSeok <ljs0429777@gmail.com>
2020-07-21 03:26:38 +0900

    2-1 mustache 활용한 동적 HTML과 MVC 패턴 생성

--- src/main/java/com/lee/practice/myslippdemo/web/WelcomeController.java
+++ src/main/java/com/lee/practice/myslippdemo/web/WelcomeController.java
@@ -3,16 +3,14 @@ package com.lee.practice.myslippdemo.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WelcomeController {
 
-    @GetMapping("/")
+    @GetMapping("/helloworld")
     public String welcome(String name, Model model) {
         System.out.println("name : " + name);
         model.addAttribute("name", name);
         return "welcome";
-    }
+        }
 }
