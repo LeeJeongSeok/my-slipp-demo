5ff839b065767c3b52d88a8846ac6152e34ed463
LeeJeongSeok <ljs0429777@gmail.com>
2020-07-21 17:54:31 +0900

    add : 2-3 사용자 목록 기능 구현

--- src/main/java/com/lee/practice/myslippdemo/web/User.java
+++ src/main/java/com/lee/practice/myslippdemo/web/User.java
@@ -21,4 +21,14 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String  toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/lee/practice/myslippdemo/web/UserController.java
+++ src/main/java/com/lee/practice/myslippdemo/web/UserController.java
@@ -1,15 +1,28 @@
 package com.lee.practice.myslippdemo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
 
+    private List<User> users = new ArrayList<User>();
+
     @PostMapping("/create")
-    public String create(String userId, String password, String name, String email) {
-        System.out.println(userId);
-        return "welcome";
+    public String create(User user) {
+        System.out.println("User : " + user);
+        users.add(user);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "list";
     }
 }
