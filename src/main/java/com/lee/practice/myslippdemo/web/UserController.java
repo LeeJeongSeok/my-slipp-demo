7d91deeb55a0e7577d340aa28c4645aca7a091cb
LeeJeongSeok <ljs0429777@gmail.com>
2020-07-23 17:28:34 +0900

    add : 새로운 html 적용 및 url 리펙토링

--- src/main/java/com/lee/practice/myslippdemo/web/UserController.java
+++ src/main/java/com/lee/practice/myslippdemo/web/UserController.java
@@ -7,26 +7,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/user/create")
+    @PostMapping("")
     public String create(User user) {
         System.out.println("User : " + user);
         userRepository.save(user);
-        return "redirect:/user/list";
+        return "redirect:/users";
     }
 
-    @GetMapping("/user/list")
+    @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
-        return "list";
+        return "/user/list";
     }
 }
