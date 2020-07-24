f916fe37f5179b2e3ab92b341a6f7e44136305b1
LeeJeongSeok <ljs0429777@gmail.com>
2020-07-25 01:14:34 +0900

    add : html 파일 중복 제거 및 개인정보 수정 기능 구현 1

--- src/main/java/com/lee/practice/myslippdemo/web/UserController.java
+++ src/main/java/com/lee/practice/myslippdemo/web/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,5 +41,13 @@ public class UserController {
     @GetMapping("/form")
     public String form() {
         return "/user/form";
+    }
+
+    @GetMapping("/{id}/form")
+    public String update_form(@PathVariable Long id, Model model) {
+        User user = userRepository.findById(id).get();
+        model.addAttribute("user", user);
+        System.out.println(user);
+        return "/user/updateForm";
     }
 }
