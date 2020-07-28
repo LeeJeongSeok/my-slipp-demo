cae2afd8060e26b47fc678275b13c8f9fe3e4592
LeeJeongSeok <ljs0429777@gmail.com>
2020-07-28 12:26:44 +0900

    add : 질문 목록 기능 구현

--- src/main/java/com/lee/practice/myslippdemo/web/HomeController.java
+++ src/main/java/com/lee/practice/myslippdemo/web/HomeController.java
@@ -1,13 +1,21 @@
 package com.lee.practice.myslippdemo.web;
 
+import com.lee.practice.myslippdemo.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("questions", questionRepository.findAll());
         return "index";
     }
+
 }
