70c7ca6671a08607f46e8d50ea11a0f8d2e1231e
LeeJeongSeok <ljs0429777@gmail.com>
2020-08-04 00:16:05 +0900

    add : 질문 상세보기 기능 추가

--- src/main/java/com/lee/practice/myslippdemo/domain/QuestionRepository.java
+++ src/main/java/com/lee/practice/myslippdemo/domain/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.lee.practice.myslippdemo.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+import java.util.Optional;
 
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Optional<Question> findById(Long id);
 }
--- src/main/java/com/lee/practice/myslippdemo/web/QuestionController.java
+++ src/main/java/com/lee/practice/myslippdemo/web/QuestionController.java
@@ -5,7 +5,9 @@ import com.lee.practice.myslippdemo.domain.QuestionRepository;
 import com.lee.practice.myslippdemo.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,4 +40,13 @@ public class QuestionController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        model.addAttribute("question", questionRepository.findById(id).get());
+
+        return "/qna/show";
+    }
+
+
 }
