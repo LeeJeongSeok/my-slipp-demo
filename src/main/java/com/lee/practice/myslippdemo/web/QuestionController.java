dc8a9117aab67940e329507bdb06a45b74854eff
LeeJeongSeok <ljs0429777@gmail.com>
2020-08-05 23:31:38 +0900

    add : Exception을 통한 중복 코드 제거

--- src/main/java/com/lee/practice/myslippdemo/web/QuestionController.java
+++ src/main/java/com/lee/practice/myslippdemo/web/QuestionController.java
@@ -28,7 +28,7 @@ public class QuestionController {
     @PostMapping("")
     public String create(String title, String contents, HttpSession session) {
         if (!HttpSessionUtils.isLoginUser(session)) {
-            return "/users/loginForm";
+            return "/user/loginForm";
         }
 
         User sessionUser = HttpSessionUtils.getUserFromSession(session);
@@ -47,53 +47,55 @@ public class QuestionController {
 
     @GetMapping("/{id}/form")
     public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
+        try {
+            Question question = questionRepository.findById(id).get();
+            hasPermission(session, question);
+            model.addAttribute("question", question);
+            return "/qna/updateForm";
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "/user/login";
+        }
+    }
+
+    private boolean hasPermission(HttpSession session, Question question) {
         if (!HttpSessionUtils.isLoginUser(session)) {
-            return "/users/loginForm";
+            throw new IllegalStateException("로그인이 필요합니다.");
         }
 
         User loginUser = HttpSessionUtils.getUserFromSession(session);
-        Question question = questionRepository.findById(id).get();
-
-        if (!question.isSameWriter(loginUser)) {
-            return "/users/loginForm";
+        if (question.isSameWriter(loginUser)) {
+            throw new IllegalStateException("자신이 쓴 글만 수정, 삭제가 가능합니다.");
         }
 
-        model.addAttribute("question", question);
-
-        return "/qna/updateForm";
+        return true;
     }
 
     @PutMapping("/{id}")
-    public String update(@PathVariable Long id, String title, String contents, HttpSession session) {
-        if (!HttpSessionUtils.isLoginUser(session)) {
-            return "/users/loginForm";
+    public String update(@PathVariable Long id, String title, String contents, HttpSession session, Model model) {
+        try {
+            Question question = questionRepository.findById(id).get();
+            hasPermission(session, question);
+            question.update(title, contents);
+            questionRepository.save(question);
+            return String.format("redirect:/questions/%d", id);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMEsagge", e.getMessage());
+            return "/user/login";
         }
-
-        User loginUser = HttpSessionUtils.getUserFromSession(session);
-        Question question = questionRepository.findById(id).get();
-
-        if (!question.isSameWriter(loginUser)) {
-            return "/users/loginForm";
-        }
-
-        question.update(title, contents);
-        questionRepository.save(question);
-        return String.format("redirect:/questions/%d", id);
     }
 
-    @DeleteMapping("/{i d}")
-    public String delete(@PathVariable Long id, HttpSession session) {
-        if (!HttpSessionUtils.isLoginUser(session)) {
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id, HttpSession session, Model model) {
+        try {
+            Question question = questionRepository.findById(id).get();
+            hasPermission(session, question);
+            model.addAttribute("question", question);
+            questionRepository.deleteById(id);
+            return "redirect:/";
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMEsagge", e.getMessage());
             return "/users/loginForm";
         }
-
-        User loginUser = HttpSessionUtils.getUserFromSession(session);
-        Question question = questionRepository.findById(id).get();
-
-        if (!question.isSameWriter(loginUser)) {
-            return "/users/loginForm";
-        }
-        questionRepository.deleteById(id);
-        return "redirect:/";
     }
 }
